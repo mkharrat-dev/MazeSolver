e15bae43970f664a42b5982eb660767af61d4753
mkharrat@outlook.fr <mkharrat@outlook.fr>
2025-07-31 00:28:07 +0200

    Update project version to execute CI/CD

--- MazeSolver.Core/obj/Debug/net8.0/MazeSolver.Core.AssemblyInfo.cs
+++ MazeSolver.Core/obj/Debug/net8.0/MazeSolver.Core.AssemblyInfo.cs
@@ -11,15 +11,13 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("MazeSolver.WpfApp")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("MazeSolver.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bf510f0b9f26df3b12713fc9ac7d71e747cd238")]
-[assembly: System.Reflection.AssemblyProductAttribute("MazeSolver.WpfApp")]
-[assembly: System.Reflection.AssemblyTitleAttribute("MazeSolver.WpfApp")]
+[assembly: System.Reflection.AssemblyProductAttribute("MazeSolver.Core")]
+[assembly: System.Reflection.AssemblyTitleAttribute("MazeSolver.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
-[assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
-[assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
 
 // Généré par la classe MSBuild WriteCodeFragment.
 
