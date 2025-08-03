840d4839f0f450a0fed035c7df37571b3db4b7ce
mkharrat@outlook.fr <mkharrat@outlook.fr>
2025-08-04 00:04:44 +0200

    Update solution

--- MazeSolver.Core/Api/MazeApiClient.cs
+++ MazeSolver.Core/Api/MazeApiClient.cs
@@ -15,19 +15,11 @@ namespace MazeSolver.Core.Api
 
         public async Task<GameState> StartGame(string player)
         {
-            //var payload = new { player };
-
-            //// Sérialiser en JSON
-            //var json = JsonSerializer.Serialize(payload);
-
-            //// Contenu HTTP avec Content-Type
-            //var content = new StringContent(json, Encoding.UTF8, "application/json");
-            var form = new MultipartFormDataContent
+            string url = BaseUrl + "start-game/";
+            MultipartFormDataContent form = new()
             {
                 { new StringContent(player), "player" }
             };
-            string url = BaseUrl + "start-game/";
-            // Requête POST avec le body JSON
             try
             {
                 var response = await _http.PostAsync(url, form);
@@ -43,8 +35,6 @@ namespace MazeSolver.Core.Api
 
         public async Task<List<Discovery>> Discover()
         {
-            var uri = State!.url_discover;
-
             var res = await _http.GetAsync(State!.url_discover);
             var body = await res.Content.ReadAsStringAsync();
             return JsonSerializer.Deserialize<List<Discovery>>(body)!;
@@ -52,15 +42,7 @@ namespace MazeSolver.Core.Api
 
         public async Task<GameState> Move(Position pos)
         {
-            var payload = new { position_x = pos.X, position_y = pos.Y };
-
-            //// Sérialiser en JSON
-            //var json = JsonSerializer.Serialize(payload);
-
-            //// Contenu HTTP avec Content-Type
-            //var content = new StringContent(json, Encoding.UTF8, "application/json");
-
-            var form = new MultipartFormDataContent
+            MultipartFormDataContent form = new()
             {
                 { new StringContent(pos.X.ToString()), "position_x" },
                 { new StringContent(pos.Y.ToString()), "position_y" }
